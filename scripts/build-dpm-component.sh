24cee77e94a9bec4321d749ea5f69aa29bff038d
Chandrakana Nandi <cnandi@cs.washington.edu>
2026-07-23 17:24:27 -0700

    clean the script

--- scripts/build-dpm-component.sh
+++ scripts/build-dpm-component.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 REPO="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+LIB="$REPO/dpm-component/lib"
 (cd "$REPO" && sbt -batch assembly)
-cp "$REPO"/target/scala-2.13/daml-analyzer-*.jar "$REPO/dpm-component/lib/daml-analyzer.jar"
+mkdir -p "$LIB"
+cp "$REPO"/target/scala-2.13/daml-analyzer-*.jar "$LIB/daml-analyzer.jar"
